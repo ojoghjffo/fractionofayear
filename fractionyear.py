111fa25ef5f7965978af35ffa49120f6fa2c25a6
ojoghjffo <94257288+ojoghjffo@users.noreply.github.com>
2024-06-01 13:36:05 -0400

    Add files via upload

---
+++ fractionyear.py
@@ -0,0 +1,44 @@
+import math
+import calendar
+
+def yeardivide():
+    try:
+        numerator = int(input("Numerator: "))
+        
+        denominator = int(input("Denominator: "))
+        leapyear = input("Add February 29? y/n ")
+        isLeapYear = leapyear in ["y", "yes"]
+        quotient = denominator / (365 + isLeapYear)
+        day = math.ceil(numerator / quotient)
+        hour = (numerator / quotient) - (math.floor(numerator / quotient))
+        minute = (hour * 24 - math.floor(hour * 24))
+        second = (minute * 60 - math.floor(minute * 60))
+        zeromin = "0" if math.floor(minute * 60) < 10 else ""
+        zerosec = "0" if math.floor(second * 60) < 10 else ""
+        if numerator < 1:
+            print("Numerator must not be less than one")
+            return
+        
+        if hour == 0:
+            hour = 24
+        
+        if numerator > denominator:
+            print("Numerator must not be greater than the Denominator")
+            return
+        cale = [31, 28 + isLeapYear, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        month = 1
+        for days in cale:
+            if day <= days:
+                print(f"{calendar.month_name[month]} {day} at {math.floor(hour * 24) if hour != 24 else hour}:{zeromin}{math.floor(minute * 60)}:{zerosec}{math.floor(second * 60)}")
+                return
+            day -= days
+            month += 1
+        
+    except ValueError:
+        print("Invalid Value")
+    except ZeroDivisionError:
+        print("Denominator cannot be zero")
+
+while True:
+    yeardivide()
+    print("")
